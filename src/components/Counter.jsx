89e5e02f94555ad980739e34ba6cb0e4143e0659
avipalerya <avipaleriya27@gmail.com>
2022-09-12 11:24:07 +0530

    first commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,15 +1,32 @@
 import React, { Component } from "react";
-
-export default class Counter extends Component {
+import { connect } from "react-redux";
+import {
+  INCREMENT_COUNTER,
+  DECREMENT_COUNTER,
+  RESET_COUNTER,
+} from "../actiontypes";
+class Counter extends Component {
   render() {
+    const increment = () => {
+      this.props.dispatch({ type: INCREMENT_COUNTER });
+    };
+    const decrement = () => {
+      this.props.dispatch({ type: DECREMENT_COUNTER });
+    };
+    const reset = () => {
+      this.props.dispatch({ type: RESET_COUNTER });
+    };
+
     console.log(this.props);
     return (
       <div>
-        <h1>Counter={this.props.counter}</h1>
-        <button>Increment</button>
-        <button>Decrement</button>
-        <button>Reset</button>
+        <h1>Counter={this.props.count}</h1>
+        <button onClick={increment}>Increment</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
       </div>
     );
   }
 }
+const mapStateToProps = (state) => ({ count: state.counterApp.counter });
+export default connect(mapStateToProps)(Counter);
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,3 +1,29 @@
+import { combineReducers } from "redux";
+import {
+  INCREMENT_COUNTER,
+  DECREMENT_COUNTER,
+  RESET_COUNTER,
+} from "../actiontypes";
 const initialState = {
   counter: 1,
 };
+export function counterApp(state = initialState, action) {
+  switch (action.type) {
+    case INCREMENT_COUNTER:
+      return {
+        counter: state.counter + 1,
+      };
+    case DECREMENT_COUNTER:
+      return {
+        counter: state.counter - 1,
+      };
+    case RESET_COUNTER:
+      return {
+        counter: initialState.counter,
+      };
+    default:
+      return state;
+  }
+}
+const rootReducer = combineReducers({ counterApp });
+export default rootReducer;
